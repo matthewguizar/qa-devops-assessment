d078b19d26a29e8dfd3ecf075c3f084385c28f0d
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-08 12:05:51 -0700

    put tests in a describe for duel duo

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -13,21 +13,25 @@ afterAll(async () => {
     driver.quit()
 })
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
+describe('test for duel duo', () => {
+    
+    test('Title shows up when page loads', async () => {
+        const title = await driver.findElement(By.id('title'))
+        const displayed = await title.isDisplayed()
+        expect(displayed).toBe(true)
+    })
+    
+    test('expecting draw button to reveal array' , async () => {
+        await driver.findElement(By.id('draw')).click()
+        expect(Array)
+        await driver.sleep(2000)
+    })
+
+    test('for see all bots', async () => {
+        await driver.findElement(By.id('see-all')).click()
+        const allBots = await driver.findElement(By.id('all-bots'))
+        expect(allBots)
+    
+    })
 })
 
-test('expecting draw button to reveal array' , async () => {
-    await driver.findElement(By.id('draw')).click()
-    expect(Array)
-    await driver.sleep(2000)
-})
-
-test('for see all bots', async () => {
-    await driver.findElement(By.id('see-all')).click()
-    const allBots = await driver.findElement(By.id('all-bots'))
-    expect(allBots)
-
-})
