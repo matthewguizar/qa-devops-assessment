a5446d9aa5926ad528909a1ddcb5a86e4011fbcb
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-08 11:43:51 -0700

    added working test functions

--- functions.test.ts
+++ functions.test.ts
@@ -1,9 +1,21 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    test('shuffles array', () => {
-       expect(shuffleArray).toBe(shuffleArray)
+    test('shuffles array should return array of the same length', () => {
+       let arr = [1, 2, 3, 4, 5]
+       let result = shuffleArray(arr)
+       expect(result.length).toEqual(arr.length)
+    })
 
-    
-})
+    test('shufflesarray should return array', () => {
+        let arr = [1, 2, 3, 4, 5]
+        let result = shuffleArray(arr)
+        expect(result).toHaveProperty('length')
+     })
+
+     test('shuffles array should return array to return an array with the same items', () => {
+        let arr = [1, 2, 3, 4, 5]
+        let result = shuffleArray(arr)
+        expect(result).toEqual(expect.arrayContaining(arr))
+     })
 })
